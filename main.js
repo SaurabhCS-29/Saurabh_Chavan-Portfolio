c54808bcec05553f054a8a578f2263e90835fac4
SaurabhCS-29 <124519125+SaurabhCS-29@users.noreply.github.com>
2023-08-04 23:31:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,18 @@ navBar.forEach(function(a){
         navCollapse.classList.remove("show");
     })
 })
+
+// Contac From send Email 
+function sendEmail(){
+    Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+}
